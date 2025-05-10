41a96efb2010a32c74b2bb3a653078d4f55514d3
Kaz <kaz@notkaz.com>
2025-05-11 01:39:40 +0200

    fixed historical

--- src/main/java/space/pxls/server/packets/http/Historical.java
+++ src/main/java/space/pxls/server/packets/http/Historical.java
@@ -7,27 +7,32 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Collections;
 
-
 public class Historical {
-	public int id;
-	public int x;
-	public int y;
-	
-	public Historical(int id, int x, int y) {
-		this.id = id;
-		this.x = x;
-		this.y = y;
-	}
+    public int id;
+    public int x;
+    public int y;
+    public List<DBPixelPlacement> placements;
 
-	public static Historical fromDB(int x, int y) {
-	    Optional<List<DBPixelPlacement>> pixelHistoryOpt = App.getDatabase().getPixelHistoryAt(x, y);
-	    
-	    return Historical.fromDB(pixelHistoryOpt.orElse(Collections.emptyList()));
-	}
+    public Historical(int id, int x, int y, List<DBPixelPlacement> placements) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.placements = placements;
+    }
 
+    public static Historical fromDB(int x, int y) {
+        Optional<List<DBPixelPlacement>> pixelHistoryOpt = App.getDatabase().getPixelHistoryAt(x, y);
+        
+        return fromDB(x, y, pixelHistoryOpt.orElse(Collections.emptyList()));
+    }
 
-	public static Historical fromDB(DBPixelPlacement pixelPlacement) {
-		if (pixelPlacement == null) return null;
-		return new Historical(pixelPlacement.id, pixelPlacement.x, pixelPlacement.y);
-	}
+    public static Historical fromDB(int x, int y, List<DBPixelPlacement> pixelHistory) {
+        if (pixelHistory == null || pixelHistory.isEmpty()) {
+            return new Historical(0, x, y, Collections.emptyList());
+        }
+
+        int historicalId = pixelHistory.get(0).id;
+        
+        return new Historical(historicalId, x, y, pixelHistory);
+    }
 }
